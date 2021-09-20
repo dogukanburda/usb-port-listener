aa92097a79ae2e115e2342e1ead89c92e143f2e9
dogukan <dogukankefeli@gmail.com>
2021-09-20 21:22:44 +0300

    usb listener without python-daemon

--- usblistener.py
+++ usblistener.py
@@ -6,10 +6,10 @@ from datetime import datetime
 from pyudev import Context, Monitor, MonitorObserver
 import subprocess
 import signal
-import os
-process_pidfile = '/home/dogukan/popenpid.pid'
-logfile = '/home/dogukan/devices.txt'
+from cysystemd import journal
 
+
+process = None
 def handle_action(device):
         """ Method to call when handling device I/O events """
         # Possible errors: if daemon detects a 'remove' event before 'add' it will fail to get a pid number
@@ -22,38 +22,23 @@ def handle_action(device):
 
         if device.action == 'add' and len(device_path)>6:
             # Logging usb events
-            try:
-                with open(logfile, 'a') as f:
-                    f.write('{0.action},{0.sys_name},{0.sys_number},{0.device_path},'.format(device))
-                    f.write(' {} \n'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-            except:
-                with open(logfile, 'w') as f:
-                    f.write('action,sys_name,sys_number,device_path,datetime')
-                    f.write('{0.action},{0.sys_name},{0.sys_number},{0.device_path},'.format(device))
-                    f.write(' {} \n'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+            journal.send(message="[ADD] Device action detected. Vendor ID : {}".format(device.get('ID_VENDOR_FROM_DATABASE')),priority=journal.Priority.INFO)
 
             # Once the USB device is plugged in, call the script and run it as a background process
-            process = subprocess.Popen(['/home/dogukan/miniconda3/bin/python3', '/home/dogukan/usb-port-listener/longscript.py'], stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT)
-            # Get the process pid and write it to a file
-            pid = process.pid
-            with open(process_pidfile, 'w') as pf:
-                pf.write(str(pid))
+            process = subprocess.Popen(['/usr/bin/python3', '/home/dogukan/usb-port-listener/longscript.py'], stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT)
+
+            # Get the process pid and log to the journal         
+            journal.send(message="A daemon process starting with PID: {}".format(str(process.pid)),priority=journal.Priority.INFO, PID=str(process.pid), PTYPE='SUB')
+
 
 
         elif device.action == 'remove' and len(device_path)>6:
             # Logging usb events
-            try:
-                with open(logfile, 'a') as f:
-                    f.write('{0.action},{0.sys_name},{0.sys_number},{0.device_path},'.format(device))
-                    f.write(' {} \n'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-            except:
-                with open(logfile, 'w') as f:
-                    f.write('action,sys_name,sys_number,device_path,datetime')
-                    f.write('{0.action},{0.sys_name},{0.sys_number},{0.device_path},'.format(device))
-                    f.write(' {} \n'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-            
+            journal.send(message="[REMOVE] Device action detected. Vendor ID : {}".format(device.get('ID_VENDOR_FROM_DATABASE')),priority=journal.Priority.INFO)
+
             # Terminate the process by its pid
             os.kill(process.pid, signal.SIGTERM) #or signal.SIGKILL
+
             time.sleep(1)
             # Retrieves the child process' state in order not to fall into zombie state 
             process.poll()
@@ -72,6 +57,8 @@ observer = MonitorObserver(monitor, callback=handle_action, name='monitor-observ
 observer.start()
 observer.join(1)
 script_pid=os.getpid()
+journal.send(message="MAIN Script is started with pid: {}".format(script_pid),priority=journal.Priority.INFO, PID=str(script_pid), PTYPE='MAIN')
+
 while True:
     print(str(script_pid))
     time.sleep(10)
